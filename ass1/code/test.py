70ba5fac4849244adfe922fa1e26f7f54c760922
AriBlumstein <ayb1998@aol.com>
2026-04-24 15:41:33 +0300

    update text files
    Signed-off-by: AriBlumstein <ayb1998@aol.com>

--- ass1/code/test.py
+++ ass1/code/test.py
@@ -49,7 +49,7 @@ test_words = [
 with open(english_file, "w") as f:
     for word in tqdm(test_words, desc="Generating samples for English words"):
         sample_text = en_tokenizer.detokenize(
-            model.better_sample_continuation(en_tokenizer.tokenize(word), max_tokens_to_generate=500, temperature=0.5, topK=5)
+            model.better_sample_continuation(en_tokenizer.tokenize("hello"), max_tokens_to_generate=500, temperature=0.5, topK=5)
             )
         f.write(sample_text + "\n\n\n\n")
 
@@ -91,7 +91,7 @@ test_words_hebrew = [
 with open(hebrew_file, "w", encoding="utf-8") as f:
     for word in tqdm(test_words_hebrew, desc="Generating samples for Hebrew words"):
         sample_text = hebrew_tokenizer.detokenize(
-            model_hebrew.better_sample_continuation(hebrew_tokenizer.tokenize(word), max_tokens_to_generate=500, temperature=0.5, topK=5)
+            model_hebrew.better_sample_continuation(hebrew_tokenizer.tokenize("שלום"), max_tokens_to_generate=500, temperature=0.5, topK=5)
             )
         f.write(sample_text + "\n\n\n\n")
 
